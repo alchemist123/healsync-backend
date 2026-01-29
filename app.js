c5d8401f7b5334fdd01b3fa3395bddbe703e6e16
Saranjith Palakkandy <saranvlmna@gmail.com>
2026-01-29 15:19:28 +0530

    cors and migrations

--- app.js
+++ app.js
@@ -3,12 +3,15 @@ var createError = require('http-errors');
 var express = require('express');
 const dotenv = require('dotenv');
 const logger = require('@middlewares/logger.middleware');
-dotenv.config();
 var app = express();
 var cors = require('cors');
+dotenv.config();
 const abhaRouter = require('./src/abha/router');
 const chatRouter = require('./src/chat/router');
 const pataiantRouter = require('./src/patiant/router');
+
+app.use(logger);
+app.use(cors());
 app.use(express.json());
 app.use(express.urlencoded({ extended: false }));
 app.set('trust proxy', true);
@@ -22,7 +25,6 @@ app.use((req, res, next) => {
   req.headers['x-browser'] = req.headers['user-agent'];
   next();
 });
-
 app.get('/health', (req, res) => {
   res.status(200).json({
     status: 'ok',
@@ -31,15 +33,12 @@ app.get('/health', (req, res) => {
     service: 'healsync-backend',
   });
 });
-
 app.get('/', (req, res) => {
   res.send('Welcome to HealSync Backend API. Use /health to check status.');
 });
-
 app.use('/abha', abhaRouter);
 app.use('/chat', chatRouter);
 app.use('/patiant', pataiantRouter);
-
 app.use(function (req, res, next) {
   next(createError(404));
 });
