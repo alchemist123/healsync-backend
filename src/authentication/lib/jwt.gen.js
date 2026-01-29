51d4e352d4129ae26c5c230e06d598ca4edcc3df
Saranjith Palakkandy <saranvlmna@gmail.com>
2026-01-29 18:19:26 +0530

    fix

--- src/authentication/lib/jwt.gen.js
+++ src/authentication/lib/jwt.gen.js
@@ -2,6 +2,9 @@ const jwt = require('jsonwebtoken');
 
 module.exports = (data) => {
   try {
+    const { user_id } = data;
+    if (!user_id) throw new Error('user_id is required to generate token');
+
     const token = jwt.sign(
       {
         exp: Math.floor(Date.now() / 1000) + 60 * 60 * 24,
